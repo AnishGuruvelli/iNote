c2ba0f5698de2b11f52d6c4b1b92f81bf1cf76b7
anish_2418 <78360526+AnishGuruvelli@users.noreply.github.com>
2021-05-22 14:07:23 +0530

    Update notetaking.js
    added the title section

--- js/notetaking.js
+++ js/notetaking.js
@@ -5,14 +5,20 @@ showNotes();
 let addBtn = document.getElementById("addBtn");
 addBtn.addEventListener("click", function (e) {
     let addTxt = document.getElementById("addTxt");
+    let addTitle = document.getElementById("addTitle");
     let notes = localStorage.getItem("notes");
     if (notes == null) {
         notesObj = [];
     } else {
         notesObj = JSON.parse(notes);
     }
-    notesObj.push(addTxt.value);
+    let myObj = {
+        title : addTitle.value,
+        text : addTxt.value,
+    }
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
+    addTitle.value = "";
     addTxt.value = "";
     //   console.log(notesObj);
     showNotes();
@@ -31,17 +37,17 @@ function showNotes() {
         html += `
             <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
                     <div class="card-body">
-                        <h5 class="card-title">Note ${index + 1}</h5>
-                        <p class="card-text"> ${element}</p>
+                        <h5 class="card-title">${element.title}</h5>
+                        <p class="card-text"> ${element.text}</p>
                         <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                     </div>
-                </div>`;
+            </div>`;
     });
     let notesElm = document.getElementById("notes");
     if (notesObj.length != 0) {
         notesElm.innerHTML = html;
     } else {
-        notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes.`;
+        notesElm.innerHTML = `Nothing to show! Use "Add Note" section above to add notes.`;
     }
 }
 
@@ -61,11 +67,13 @@ function deleteNote(index) {
     showNotes();
 }
 
+// Function to search for a keyword
 
 let search = document.getElementById('searchTxt');
 search.addEventListener("input", function () {
 
-    let inputVal = search.value.toLowerCase();
+    // let inputVal = search.value.toLowerCase();
+    let inputVal = search.value();
     // console.log('Input event fired!', inputVal);
     let noteCards = document.getElementsByClassName('noteCard');
     Array.from(noteCards).forEach(function (element) {
@@ -83,9 +91,6 @@ search.addEventListener("input", function () {
 /*
 
 Further Features:
-1. Add Title
-2. Mark a note as Important
-3. Separate notes by user
-4. Sync and host to web server 
+1. Mark a note as Important
 
 */
